81b2a4e7a4d46bfba1f2beceeb84aac01d9f49ab
misriii <misriyaamman10@gmail.com>
2023-09-30 18:44:32 +0530

    height and currency added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: any = 0;
 
-  name: string='';
-  date: string='';
-
-  OnNameChange(value: string){
-    this.name=value
+  OnNameChange(value: string) {
+    this.name = value;
   }
 
-  OnDateChange(value:string){
-    this.date=value
+  OnDateChange(value: string) {
+    this.date = value;
+  }
+
+  OnAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  OnHeightChange(value: string) {
+    this.height = value;
   }
 }
